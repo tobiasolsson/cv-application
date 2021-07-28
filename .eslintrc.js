f42da18f910b31f638bd404dedf3b42e06190fc6
Tobias Olsson <tobias.olsson@pm.me>
2021-07-28 11:07:45 +0200

    Fix prettier syntax rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,11 @@ module.exports = {
     jest: true,
     node: true,
   },
-  parser: "babel-eslint",
-  extends: ["airbnb", "prettier"],
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
@@ -17,14 +17,14 @@ module.exports = {
       modules: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "react/forbid-prop-types": [0, { forbid: ["any"] }],
-    "react/prop-types": 0,
-    "react/destructuring-assignment": 1,
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/forbid-prop-types': [0, { forbid: ['any'] }],
+    'react/prop-types': 0,
+    'react/destructuring-assignment': 1,
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
+import Form from './components/editSection/form/Form';
 
 function App() {
   return (
     <div>
       <p>CV Application</p>
+      <Form />
     </div>
   );
 }
